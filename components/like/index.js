a657c27a87dab83310237c09e4e884ad8ee92b2f
sangruiheng <919915113@qq.com>
2019-07-16 23:26:55 +0800

    喜欢

--- components/like/index.js
+++ components/like/index.js
@@ -1,16 +1,23 @@
 // components/like/index.js
 Component({
   /**
-   * 组件的属性列表
+   * 组件的属性列表 (从外部传来的属性)
    */
   properties: {
-
+    like: {
+      type: Boolean,
+    },
+    count: {
+      type: Number,
+    }
   },
 
   /**
-   * 组件的初始数据
+   * 组件的初始数据 (内部属性)
    */
   data: {
+    yesSrc: 'images/like.png',
+    noSrc: 'images/like@dis.png'
 
   },
 
@@ -18,6 +25,15 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    onLike: function (event) {
+      let like = this.properties.like
+      let count = this.properties.count
+      count = like ? count - 1 : count + 1
+      console.log(count)
+      this.setData({
+        count: count,
+        like: !like
+      })
+    }
   }
 })
